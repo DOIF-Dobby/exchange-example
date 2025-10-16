9b0209e077df17c3893be806e009bdd16b4c26ce
Kim-MyungJin <myungjin.kim@switchwon.com>
2025-10-16 15:14:37 +0900

    first commit

---
+++ src/main/kotlin/com/switchwon/exchange/exchangeexample/ExchangeExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.switchwon.exchange.exchangeexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExchangeExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExchangeExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/switchwon/exchange/exchangeexample/ExchangeExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.switchwon.exchange.exchangeexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExchangeExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
